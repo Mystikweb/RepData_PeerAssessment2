e6d701ec00fe2a8e32c3ccdbe2b440eecf4adb70
Christopher Hair <mystikweb@live.ca>
2017-06-25 14:36:51 -0400

    Updating the sandbox to get the health information ready for the report and building the data preperation section of the report

--- sandbox.R
+++ sandbox.R
@@ -1,31 +1,47 @@
-if (!exists("dataset")) {
-    dataset <- read.csv("data/repdata_data_StormData.csv.bz2")  
+if (!exists("raw_data")) {
+    column_classes <- c(rep("NULL", 7), "character",
+                        rep("NULL", 14), "numeric", "numeric",
+                        "numeric", "character", "numeric", "character",
+                        rep("NULL", 9))
+    raw_data <- read.csv("data/repdata_data_StormData.csv.bz2",
+                         colClasses = column_classes)
 }
-
-event_levels <- levels(dataset$EVTYPE)
 
 library(dplyr)
 
-event_injuries <- dataset %>%
+event_injuries <- raw_data %>%
     group_by(EVTYPE) %>%
     summarise(total_events = n(),
               avg_injuries = mean(INJURIES),
               total_injuries = sum(INJURIES)) %>%
     filter(total_events > 100) %>%
     arrange(desc(total_injuries)) %>%
-    select(event_type = EVTYPE, total_events, avg_injuries, total_injuries)
+    select(event_type = EVTYPE, 
+           total_events, 
+           avg_injuries, 
+           total_injuries)
 
-event_fatalities <- dataset %>%
+event_fatalities <- raw_data %>%
     group_by(EVTYPE) %>%
     summarise(total_events = n(),
               avg_fatalities = mean(FATALITIES),
               total_fatalities = sum(FATALITIES)) %>%
     filter(total_events > 100) %>%
     arrange(desc(total_fatalities)) %>%
-    select(event_type = EVTYPE, total_events, avg_fatalities, total_fatalities)
+    select(event_type = EVTYPE, 
+           total_events, 
+           avg_fatalities, 
+           total_fatalities)
 
-event_data <- event_injuries %>%
+health_data <- event_injuries %>%
     inner_join(event_fatalities) %>%
-    filter((avg_injuries > 1 & avg_fatalities != 0) | (avg_fatalities > 1 & avg_injuries != 0)) %>%
-    filter(avg_injuries != total_injuries & avg_fatalities != total_fatalities) %>%
-    select(event_type, total_events, total_injuries, total_fatalities)
+    filter((avg_injuries > 1 & avg_fatalities != 0) | 
+               (avg_fatalities > 1 & avg_injuries != 0)) %>%
+    filter(avg_injuries != total_injuries & 
+               avg_fatalities != total_fatalities) %>%
+    mutate(total_health_related = total_injuries + total_fatalities) %>%
+    select(event_type, 
+           total_events, 
+           total_injuries, 
+           total_fatalities, 
+           total_health_related)
